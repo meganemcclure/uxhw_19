98472326b6a5703bc91da0e6ecc37000bf0507da
Megan McClure <meganemcclure@gmail.com>
2023-08-01 18:39:30 -0400

    improve responsiveness

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 $(".condensedMenuButton").on("click", function() {
     if ($(".condensedNav").hasClass("open")) {
         $(".condensedNav").removeClass("open")
-        $(".condensedNav").animate({height: "120px"})
+        $(".condensedNav").animate({height: "80px"})
         $(".condensedNavContents").slideUp("fast")
     } else {
         $(".condensedNav").addClass("open")
